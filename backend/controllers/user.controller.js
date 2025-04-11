62eb72795b6f84e7a2b3107322d3c121a9d0989a
yashash ravi <yashashravi1@gmail.com>
2025-04-11 20:54:58 +0530

    modified:   backend/controllers/user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 const userModel = require('../models/user.model');
 
 module.exports.register_user =(req,res,next)=>{
+    const {username,password,} = req.body;
+
 
 }
 
